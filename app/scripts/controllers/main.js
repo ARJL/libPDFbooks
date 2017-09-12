92da07e0e42aeac0e26d928ed5887e4bab763414
jelassi <arbia.jalassi@supcom.tn>
2017-09-12 19:23:00 +0200

    StartingProject

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name libPdfbooksApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the libPdfbooksApp
+ */
+angular.module('libPdfbooksApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
